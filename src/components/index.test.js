68ad34ba920ce110c21f9d4cf6e29830f90a3555
occammedia <carl.p@freshly-squeezed.com>
2018-12-03 15:55:41 +0000

    Added enzyme flow types

--- src/components/index.test.js
+++ src/components/index.test.js
@@ -1,3 +1,5 @@
+// @flow
+
 import * as React from 'react'
 import {shallow} from 'enzyme'
 
@@ -5,7 +7,7 @@ import DocumentComponent from './index'
 
 describe('<DocumentComponent/>', (): void => {
   test('Should render the title and slug of the project', (): void => {
-    const wrapper: ShallowWrapper<React.ReactElement<DocumentComponent>> = shallow(<DocumentComponent slug="test"/>)
+    const wrapper: ShallowWrapper = shallow(<DocumentComponent slug="test"/>)
 
     expect(wrapper.find('.document-component')).toHaveLength(1)
   })
